772ac4cd4c2f6d413a96ebcc2c73e36448a5c1ff
변준영 <junyung001@naver.com>
2026-08-21 02:55:12 +0900

    fix: 루틴 저장 요청의 routines 검증 허용
    전역 ValidationPipe에서 routines 필드가 허용되지 않은 값으로 처리되던 문제를 수정했다.
    
    보유 제품 기반 루틴 생성과 트러블 상담 조정 루틴 저장 요청이 모두 정상 처리되도록 배열 검증을 추가했다.

--- src/app/user/routine/dto/patch_user_routine.request.dto.ts
+++ src/app/user/routine/dto/patch_user_routine.request.dto.ts
@@ -1,5 +1,8 @@
 import { CareRoutine } from 'src/app/care_routine/type/care_routine.type';
+import { IsArray, IsOptional } from 'class-validator';
 
 export class PatchUserRoutineRequestDto {
+  @IsOptional()
+  @IsArray()
   routines?: CareRoutine['routines'];
 }
